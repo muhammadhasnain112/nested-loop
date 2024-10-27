b2e1ca4254d05260f959ed69f3bba788b27c791b
muhammadhasnain112 <muhammadhasnain12446@gmail.com>
2024-10-27 18:34:38 +0500

    nested loops

---
+++ app.js
@@ -0,0 +1,11 @@
+for (let i = '*'; i.length <= 10; i += '*') {
+    document.write(i);
+    document.write('<br>')
+}
+
+
+for (let j = '$'; j.length <= 10; j += '$') {
+    document.write('<br> <center>');
+    document.write((j));
+
+}
